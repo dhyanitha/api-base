2c2b27ab184700e8ad130c3341a012058945da63
Alberto Basalo <albertobasalo@agorabinaria.com>
2017-07-17 12:04:50 +0200

    init

---
+++ server/src/app/core/shared/enums.ts
@@ -0,0 +1,19 @@
+export enum ROLE {
+  ADMIN = 'ADMIN',
+  CLIENT = 'CLIENT',
+  GOD = 'GOD',
+  PUBLIC = 'PUBLIC',
+}
+
+export enum STATUS {
+  PENDING = 'PENDING',
+  CONFIRMED = 'CONFIRMED',
+  ACTIVE = 'ACTIVE',
+  DISABLED = 'DISABLED',
+  CANCELED = 'CANCELED'
+}
+
+export enum STAGES {
+  prod = 'prod',
+  dev = 'dev'
+}
