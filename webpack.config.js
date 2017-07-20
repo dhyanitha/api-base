b69ee9975326ae187a0b884640b4468ecab502bf
silvelo <arturo.silvelo@gmail.com>
2017-07-20 14:02:17 +0200

    Build Sucess wiht auto zip

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,31 @@
+var nodeExternals = require('webpack-node-externals');
 var webpack = require('webpack');
 var path = require('path');
 var fs = require('fs');
 
-var nodeModules = {};
-fs.readdirSync('node_modules')
-    .filter(function (x) {
-        return ['.bin'].indexOf(x) === -1;
-    })
-    .forEach(function (mod) {
-        nodeModules[mod] = 'commonjs ' + mod;
-    });
 
 module.exports = {
-    entry: './src/main.ts',
-    target: 'node',
-    output: {
-        path: path.join(__dirname, '..', 'dist/server'),
-        filename: 'backend.js'
-    },
-    resolve: {
-        extensions: [".ts", ".js"]
-    },
-    module: {
-        loaders: [
-            { test: /\.ts?$/, loader: "ts-loader" }
-        ]
-    },
-    plugins: [
-        new webpack.BannerPlugin({ banner: 'require("source-map-support").install();', raw: true, entryOnly: false })
-    ],
-    devtool: 'sourcemap',
-    externals: nodeModules
+  entry: './dist/server/main.js',
+  target: 'node',
+  output: {
+    path: path.join(__dirname, 'compile'),
+    filename: '[name]-backend.js',
+    libraryTarget: 'commonjs2'
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+    modules: [
+      'node_modules'
+    ]
+  },
+  module: {
+    loaders: [
+      { test: /\.ts?$/, loader: "ts-loader" }
+    ]
+  },
+  plugins: [
+    new webpack.BannerPlugin({ banner: 'require("source-map-support").install();', raw: true, entryOnly: false })
+  ],
+  devtool: 'sourcemap',
+  externals: [nodeExternals()]
 }
